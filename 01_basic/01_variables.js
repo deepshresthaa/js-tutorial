233e16262104039d2285054b4872b897a023af90
deepshresthaa <147686787+deepshresthaa@users.noreply.github.com>
2023-10-12 06:41:51 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=12321;
+let accountEmail="dipstha1313@gmail.com";
+var accountOwner="Deep Shrestha";
+accountEmail="dip@gmail.com";
+accountOwner="Anjali Shrestha";
+console.log(accountId);
+console.table([accountId, accountEmail, accountOwner])
