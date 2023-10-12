0676b0d45e32f82175849d431ae25b4488172938
deepshresthaa <147686787+deepshresthaa@users.noreply.github.com>
2023-10-12 07:36:22 +0000

    variables

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -4,4 +4,12 @@ var accountOwner="Deep Shrestha";
 accountEmail="dip@gmail.com";
 accountOwner="Anjali Shrestha";
 console.log(accountId);
-console.table([accountId, accountEmail, accountOwner])
+console.table([accountId, accountEmail, accountOwner]);
+
+
+//Notes:
+/*
+1.)const datatype for those values which never changes in future
+2.)var is datatype of older version of js, and let is newer versioned so it is preferred to use let rather than var.
+
+*/
