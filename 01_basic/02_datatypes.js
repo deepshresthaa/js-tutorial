38bad67a260bdcd728866346fba4ca778af5fa06
deepshresthaa <147686787+deepshresthaa@users.noreply.github.com>
2023-10-12 06:50:50 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // cant use alert because this is node.js alert; can be used in browser
+console.log(3+3);
+console.log("Deep");
+let name="Deep";
+let age=19;
+let isLoggedIn=false;
+
+// datatypes
+// number => range(2 to power 53)
+// bigint => for huge websites like facebook
+// string=>" deep"
+// boolean=>true/false
+//null => standalone value
+// undefined
+// symbol => for uniqueness in react
+
+console.log(typeof "Deep");// string type
+console.log(typeof age);// number type
+console.log(typeof null); // object type
+console.log(typeof undefined);//undefined type
