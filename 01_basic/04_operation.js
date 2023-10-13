9129017c92669e60e891adb8ebeddfb02db0ea9c
deepshresthaa <147686787+deepshresthaa@users.noreply.github.com>
2023-10-13 05:19:56 +0000

    operations

---
+++ 01_basic/04_operation.js
@@ -0,0 +1,17 @@
+// *******Operations*********
+let value=3;
+let negValue=-value;
+console.log(negValue);
+ 
+// arithmetic operations
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+
+// tricky operations
+console.log(1 + "2");
+console.log( "1" + 2);
+console.log(1 + 2 + "3"); // first mathematical operation which comes first before string 
+console.log("1" + 2 + 3);  // here string comes before mathematical operations so , all number is converted to string
