4b8aa3ebe9c86bfb51eb7937f084b6eda54bfe42
deepshresthaa <147686787+deepshresthaa@users.noreply.github.com>
2023-10-30 12:07:41 +0000

    while loop in js

---
+++ 05_iteration/02_whileAnddowhile.js
@@ -0,0 +1,26 @@
+// while loop
+let i=1; 
+while(i<=7)
+{
+    console.log(i);
+    i++;
+}
+
+// in array
+let arr=["pikachu","mewto","caesaro"];
+let j=0;
+while(j<arr.length)
+{
+    console.log(`Pokemons: ${arr[j]}`);
+    j++;
+}
+
+
+//do while loop
+console.log("demonstrating do while loop::::");
+let k=1;
+do{
+    console.log(k);
+    k++;
+}
+while(k<=4)
