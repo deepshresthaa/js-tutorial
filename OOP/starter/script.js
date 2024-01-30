4820cc011e477ff551f46267f47b8f12a6593d7f
deepshresthaa <147686787+deepshresthaa@users.noreply.github.com>
2024-01-30 14:01:08 +0000

    OOP

--- OOP/starter/script.js
+++ OOP/starter/script.js
@@ -26,3 +26,18 @@ console.log(Person.prototype.isPrototypeOf(deep));
 //check if property is its own or not
 console.log(deep.hasOwnProperty("firstName"));
 console.log(deep.hasOwnProperty("species"));
+
+//prototypal inheritance and prototypal chaining
+//Person.prototype is not prototype of Person but of all the objects created using Person constructor
+//prototypal chaining is same as scope chaining but with .__proto__
+
+//prototypal inheritance on built-in objects
+const arr=[2,3,4,4,3];
+Array.prototype.unique=function(){
+    console.log([...new Set(this)]);
+}
+arr.unique();
+console.log(arr)
+console.log(arr.__proto__)//methods and properties that arr can inherit and in Array.prototype
+console.log(arr.__proto__.__proto__)// constructor.object()
+console.log(arr.__proto__.__proto__.__proto__)// no further chaining after Constructor.object so gives null
